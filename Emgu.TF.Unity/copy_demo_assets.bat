2320486529ab225b159861cd7b83961227571f69
Canming Huang <support@emgu.com>
2017-06-28 11:50:18 -0400

    Update for Unity packaging.

--- Emgu.TF.Unity/copy_demo_assets.bat
+++ Emgu.TF.Unity/copy_demo_assets.bat
@@ -1,8 +1,2 @@
-rm -rf Assets/Emgu.TF/Emgu.TF.Demo
-REM mkdir Assets\Emgu.TF\Emgu.TF.Demo
-mkdir -p Assets\Emgu.TF\Emgu.TF.Demo\Resources
-
 cp ../tensorflow/tensorflow/examples/multibox_detector/data/surfers.jpg Assets/Emgu.TF/Emgu.TF.Demo/Resources
-
-cp -rf demo/* Assets/Emgu.TF/Emgu.TF.Demo
 cp unityStoreIcons/EmguTFLogo_128x128.png Assets/Emgu.TF/Emgu.TF.Demo/EmguTFLogo.png
