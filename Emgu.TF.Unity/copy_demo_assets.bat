d9923b2f67f3d42e44953e49b52c1efae7358f6a
Canming Huang <support@emgu.com>
2017-06-28 09:57:33 -0400

    update unity scripts for OSX

--- Emgu.TF.Unity/copy_demo_assets.bat
+++ Emgu.TF.Unity/copy_demo_assets.bat
@@ -1,9 +1,8 @@
 rm -rf Assets/Emgu.TF/Emgu.TF.Demo
-mkdir Assets\Emgu.TF\Emgu.TF.Demo
-mkdir Assets\Emgu.TF\Emgu.TF.Demo\Resources
+REM mkdir Assets\Emgu.TF\Emgu.TF.Demo
+mkdir -p Assets\Emgu.TF\Emgu.TF.Demo\Resources
 
 cp ../tensorflow/tensorflow/examples/multibox_detector/data/surfers.jpg Assets/Emgu.TF/Emgu.TF.Demo/Resources
-
 
 cp -rf demo/* Assets/Emgu.TF/Emgu.TF.Demo
 cp unityStoreIcons/EmguTFLogo_128x128.png Assets/Emgu.TF/Emgu.TF.Demo/EmguTFLogo.png
