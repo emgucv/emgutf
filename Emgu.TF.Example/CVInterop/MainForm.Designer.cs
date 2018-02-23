4ccd6fb5f60124401f4cd199139f13431cdb3786
Canming Huang <supprt@emgu.com>
2018-02-23 11:39:53 -0500

    Added option to use camera to perform object recognition.

--- Emgu.TF.Example/CVInterop/MainForm.Designer.cs
+++ Emgu.TF.Example/CVInterop/MainForm.Designer.cs
@@ -32,21 +32,24 @@
             this.messageLabel = new System.Windows.Forms.Label();
             this.fileNameTextBox = new System.Windows.Forms.TextBox();
             this.openFileButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cameraButton = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox
             // 
-            this.pictureBox.Location = new System.Drawing.Point(12, 134);
+            this.pictureBox.Location = new System.Drawing.Point(12, 193);
             this.pictureBox.Name = "pictureBox";
-            this.pictureBox.Size = new System.Drawing.Size(1209, 951);
+            this.pictureBox.Size = new System.Drawing.Size(1209, 892);
             this.pictureBox.TabIndex = 0;
             this.pictureBox.TabStop = false;
             // 
             // messageLabel
             // 
             this.messageLabel.AutoSize = true;
-            this.messageLabel.Location = new System.Drawing.Point(13, 82);
+            this.messageLabel.Location = new System.Drawing.Point(13, 149);
             this.messageLabel.Name = "messageLabel";
             this.messageLabel.Size = new System.Drawing.Size(113, 20);
             this.messageLabel.TabIndex = 1;
@@ -54,7 +57,7 @@
             // 
             // fileNameTextBox
             // 
-            this.fileNameTextBox.Location = new System.Drawing.Point(17, 31);
+            this.fileNameTextBox.Location = new System.Drawing.Point(183, 24);
             this.fileNameTextBox.Name = "fileNameTextBox";
             this.fileNameTextBox.ReadOnly = true;
             this.fileNameTextBox.Size = new System.Drawing.Size(683, 26);
@@ -62,7 +65,7 @@
             // 
             // openFileButton
             // 
-            this.openFileButton.Location = new System.Drawing.Point(716, 27);
+            this.openFileButton.Location = new System.Drawing.Point(882, 20);
             this.openFileButton.Name = "openFileButton";
             this.openFileButton.Size = new System.Drawing.Size(56, 35);
             this.openFileButton.TabIndex = 3;
@@ -70,11 +73,42 @@
             this.openFileButton.UseVisualStyleBackColor = true;
             this.openFileButton.Click += new System.EventHandler(this.openFileButton_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Recognize image file:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(193, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Recognize camera image:";
+            // 
+            // cameraButton
+            // 
+            this.cameraButton.Location = new System.Drawing.Point(216, 88);
+            this.cameraButton.Name = "cameraButton";
+            this.cameraButton.Size = new System.Drawing.Size(179, 35);
+            this.cameraButton.TabIndex = 6;
+            this.cameraButton.Text = "Start Camera";
+            this.cameraButton.UseVisualStyleBackColor = true;
+            this.cameraButton.Click += new System.EventHandler(this.cameraButton_Click);
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1233, 1110);
+            this.ClientSize = new System.Drawing.Size(1252, 1110);
+            this.Controls.Add(this.cameraButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.openFileButton);
             this.Controls.Add(this.fileNameTextBox);
             this.Controls.Add(this.messageLabel);
@@ -93,6 +127,9 @@
         private System.Windows.Forms.Label messageLabel;
         private System.Windows.Forms.TextBox fileNameTextBox;
         private System.Windows.Forms.Button openFileButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button cameraButton;
     }
 }
 
--- Emgu.TF.Example/CVInterop/MainForm.cs
+++ Emgu.TF.Example/CVInterop/MainForm.cs
@@ -17,59 +17,80 @@ namespace CVInterop
 {
     public partial class MainForm : Form
     {
+        private Inception inceptionGraph;
+
         public MainForm()
         {
             InitializeComponent();
 
             TfInvoke.CheckLibraryLoaded();
             messageLabel.Text = String.Empty;
+            cameraButton.Text = "Start Camera";
+
+            //Use the following code for the full inception model
+            System.Net.ServicePointManager.Expect100Continue = true;
+            System.Net.ServicePointManager.SecurityProtocol = System.Net.SecurityProtocolType.Tls12;
+            inceptionGraph = new Inception();
 
             Recognize("space_shuttle.jpg");
 
+            
+        }
+
+        public void Recognize(Mat m)
+        {
+            Tensor imageTensor = Emgu.TF.TensorConvert.ReadTensorFromMatBgr(m, 224, 224, 128.0f, 1.0f / 128.0f);
+
+            //Uncomment the following code to use a retrained model to recognize followers, downloaded from the internet
+            //Inception inceptionGraph = new Inception(null, new string[] {"optimized_graph.pb", "output_labels.txt"}, "https://github.com/emgucv/models/raw/master/inception_flower_retrain/", "Mul", "final_result");
+            //Tensor imageTensor = ImageIO.ReadTensorFromMatBgr(fileName, 299, 299, 128.0f, 1.0f / 128.0f);
+
+            //Uncomment the following code to use a retrained model to recognize followers, if you deployed the models with the application
+            //For ".pb" and ".txt" bundled with the application, set the url to null
+            //Inception inceptionGraph = new Inception(null, new string[] {"optimized_graph.pb", "output_labels.txt"}, null, "Mul", "final_result");
+            //Tensor imageTensor = ImageIO.ReadTensorFromMatBgr(fileName, 299, 299, 128.0f, 1.0f / 128.0f);
+
+            Stopwatch sw = Stopwatch.StartNew();
+            float[] probability = inceptionGraph.Recognize(imageTensor);
+            sw.Stop();
+
+            String resStr = String.Empty;
+            if (probability != null)
+            {
+                String[] labels = inceptionGraph.Labels;
+                float maxVal = 0;
+                int maxIdx = 0;
+                for (int i = 0; i < probability.Length; i++)
+                {
+                    if (probability[i] > maxVal)
+                    {
+                        maxVal = probability[i];
+                        maxIdx = i;
+                    }
+                }
+                resStr = String.Format("Object is {0} with {1}% probability. Recognition completed in {2} milliseconds.", labels[maxIdx], maxVal * 100, sw.ElapsedMilliseconds);
+            }
+
+            if (InvokeRequired)
+            {
+                this.Invoke( (MethodInvoker)  (() =>
+                   {
+                       messageLabel.Text = resStr;
+                       pictureBox.Image = m.Bitmap;
+                   }));
+            }
         }
 
 
         public void Recognize(String fileName)
         {
             fileNameTextBox.Text = fileName;
-            pictureBox.ImageLocation = fileName;
+            
             
             using (Mat m = CvInvoke.Imread(fileName))
             {
-                //Use the following code for the full inception model
-                Inception inceptionGraph = new Inception();
-                Tensor imageTensor = Emgu.TF.TensorConvert.ReadTensorFromMatBgr(m, 224, 224, 128.0f, 1.0f / 128.0f);
-
-                //Uncomment the following code to use a retrained model to recognize followers, downloaded from the internet
-                //Inception inceptionGraph = new Inception(null, new string[] {"optimized_graph.pb", "output_labels.txt"}, "https://github.com/emgucv/models/raw/master/inception_flower_retrain/", "Mul", "final_result");
-                //Tensor imageTensor = ImageIO.ReadTensorFromMatBgr(fileName, 299, 299, 128.0f, 1.0f / 128.0f);
-
-                //Uncomment the following code to use a retrained model to recognize followers, if you deployed the models with the application
-                //For ".pb" and ".txt" bundled with the application, set the url to null
-                //Inception inceptionGraph = new Inception(null, new string[] {"optimized_graph.pb", "output_labels.txt"}, null, "Mul", "final_result");
-                //Tensor imageTensor = ImageIO.ReadTensorFromMatBgr(fileName, 299, 299, 128.0f, 1.0f / 128.0f);
-
-                Stopwatch sw = Stopwatch.StartNew();
-                float[] probability = inceptionGraph.Recognize(imageTensor);
-                sw.Stop();
-
-                String resStr = String.Empty;
-                if (probability != null)
-                {
-                    String[] labels = inceptionGraph.Labels;
-                    float maxVal = 0;
-                    int maxIdx = 0;
-                    for (int i = 0; i < probability.Length; i++)
-                    {
-                        if (probability[i] > maxVal)
-                        {
-                            maxVal = probability[i];
-                            maxIdx = i;
-                        }
-                    }
-                    resStr = String.Format("Object is {0} with {1}% probability. Recognition completed in {2} milliseconds.", labels[maxIdx], maxVal * 100, sw.ElapsedMilliseconds);
-                }
-                messageLabel.Text = resStr;
+                Recognize(m);
+                
             }
         }
 
@@ -83,6 +104,42 @@ namespace CVInterop
                 String fileName = ofd.FileName;
                 Recognize(fileName);
             }
+        }
+
+        private VideoCapture _capture = null;
+
+        private void cameraButton_Click(object sender, EventArgs e)
+        {
+            if (cameraButton.Text.Equals("Start Camera"))
+            {
+                if (_capture == null)
+                {
+                    _capture = new VideoCapture(0);
+                    if (!_capture.IsOpened)
+                    {
+                        _capture = null;
+                        MessageBox.Show("Failed to open camera");
+                        return;
+                    }      
+                    _capture.ImageGrabbed += _capture_ImageGrabbed;
+                    _capture.Start();
+                }
+                cameraButton.Text = "Stop Camera";   
+            } else
+            {
+                _capture.Stop();
+                _capture.Dispose();
+                _capture = null;
+                cameraButton.Text = "Start Camera";
+            }
+        }
+
+        private Mat _captureFrame = new Mat();
+
+        private void _capture_ImageGrabbed(object sender, EventArgs e)
+        {
+            _capture.Retrieve(_captureFrame);
+            Recognize(_captureFrame);
         }
     }
 }
