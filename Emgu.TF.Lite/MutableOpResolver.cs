c875bdf6f3739e0092d41fce6565a1342b9970e5
Canming Huang <supprt@emgu.com>
2018-05-08 16:12:14 -0400

    Added MutableOpResolver.

---
+++ Emgu.TF.Lite/MutableOpResolver.cs
@@ -0,0 +1,53 @@
+﻿//----------------------------------------------------------------------------
+//  Copyright (C) 2004-2018 by EMGU Corporation. All rights reserved.       
+//----------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using Emgu.TF.Util;
+
+namespace Emgu.TF.Lite
+{
+    
+    public class MutableOpResolver : Emgu.TF.Util.UnmanagedObject, IOpResolver
+    {
+        private IntPtr _opResolverPtr;
+        
+        public MutableOpResolver()
+        {
+            _ptr = TfLiteInvoke.tfeMutableOpResolverCreate(ref _opResolverPtr);
+        }
+        
+        IntPtr IOpResolver.OpResolverPtr
+        {
+            get
+            {
+                return _opResolverPtr;
+            }
+        }
+
+        /// <summary>
+        /// Release all the unmanaged memory associated with this model
+        /// </summary>
+        protected override void DisposeObject()
+        {
+            if (IntPtr.Zero != _ptr)
+            {
+                TfLiteInvoke.tfeMutableOpResolverRelease(ref _ptr);
+                _opResolverPtr = IntPtr.Zero;
+            }
+        }
+    }
+
+    public static partial class TfLiteInvoke
+    {
+        [DllImport(ExternLibrary, CallingConvention = TfLiteInvoke.TFCallingConvention)]
+        internal static extern IntPtr tfeMutableOpResolverCreate(ref IntPtr opResolver);
+
+        [DllImport(ExternLibrary, CallingConvention = TfLiteInvoke.TFCallingConvention)]
+        internal static extern void tfeMutableOpResolverRelease(ref IntPtr resolver);
+
+    }
+}
