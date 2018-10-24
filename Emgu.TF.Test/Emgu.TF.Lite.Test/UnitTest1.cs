9b51cdc05dde942566194c8253c17de0bc26f3d6
Canming Huang <supprt@emgu.com>
2018-10-24 10:09:56 -0400

    Added UnitTest for Emgu.TF.Lite.

--- Emgu.TF.Test/Emgu.TF.Lite.Test/UnitTest1.cs
+++ Emgu.TF.Test/Emgu.TF.Lite.Test/UnitTest1.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Emgu.TF.Lite;
+using Emgu.TF.Lite.Models;
+using System.Threading;
 
 namespace Emgu.TF.Lite.Test
 {
@@ -8,9 +10,30 @@ namespace Emgu.TF.Lite.Test
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestGetVersion()
         {
+            String version = TfLiteInvoke.Version;
+        }
 
+        [TestMethod]
+        public void TestMobilenet()
+        {
+            using (Mobilenet mobilenet = new Mobilenet())
+            {
+                bool processCompleted = false;
+                mobilenet.OnDownloadCompleted += (sender, e) =>
+                {
+                    var result = mobilenet.MostLikely("grace_hopper.jpg");
+                    
+                    processCompleted = true;
+                };
+
+                mobilenet.Init();
+                while (!processCompleted)
+                {
+                    Thread.Sleep(1000);
+                }
+            }
         }
     }
 }
