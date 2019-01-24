eef59645c1482b81e5cc303c59f61b9d57dcc75b
Canming Huang <supprt@emgu.com>
2019-01-24 12:52:46 -0500

    Copy over the space_shuttle image for Unity demo.

--- Emgu.TF.Unity/copy_demo_assets.bat
+++ Emgu.TF.Unity/copy_demo_assets.bat
@@ -1,2 +1,3 @@
 cp ../tensorflow/tensorflow/examples/multibox_detector/data/surfers.jpg Assets/Emgu.TF/Demo/Resources
+cp ../Emgu.TF.Example/InceptionObjectRecognition/space_shuttle.jpg Assets/Emgu.TF/Demo/Resources
 cp unityStoreIcons/EmguTFLogo_128x128.png Assets/Emgu.TF/Demo/EmguTFLogo.png
