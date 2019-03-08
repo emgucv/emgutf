7c8d0a6572884031d3057b9513965fd040e57bef
Canming Huang <supprt@emgu.com>
2019-03-08 13:18:49 -0500

    Build unity document using WSL.

--- Emgu.TF.Lite.Unity/build-unity-doc.bat
+++ Emgu.TF.Lite.Unity/build-unity-doc.bat
@@ -1,9 +1,9 @@
 REM go to the folder of the current script
 pushd %~p0
 cp NamespaceDoc.cs Assets/Emgu.TF.Lite/Assets/Scripts
-doxygen Emgu.TF.Lite.Doxyfile
+wsl doxygen Emgu.TF.Lite.Doxyfile
 cd latex
-call make
+wsl make
 cd ..
 cp latex/refman.pdf ../Emgu.TF.Lite.Unity/Assets/Emgu.TF.Lite/Documentation/Emgu.TF.Lite.Documentation.pdf
 rm Assets/Emgu.TF.Lite/Assets/Scripts/NamespaceDoc.cs
