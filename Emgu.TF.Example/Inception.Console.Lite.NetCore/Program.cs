fa581b4799702e0681faae0cbb3be52145e61dd0
Canming Huang <supprt@emgu.com>
2019-05-24 08:08:01 -0400

    Build the .Net Core Console app with CMake.

--- Emgu.TF.Example/Inception.Console.Lite.NetCore/Program.cs
+++ Emgu.TF.Example/Inception.Console.Lite.NetCore/Program.cs
@@ -12,31 +12,31 @@ namespace Inception.Console.Lite.Netstandard
 {
     class Program
     {
-        private static Inception inception;
+        private static Emgu.TF.Lite.Models.Inception inception;
         private static String fileName = "tulips.jpg";
         static void Main(string[] args)
         {
             new Thread(() => { Run(); }).Start();
 
-            Console.ReadKey();
+            System.Console.ReadKey();
         }
 
         private static void Run()
         {
-            inception = new Inception();
+            inception = new Emgu.TF.Lite.Models.Inception();
             inception.OnDownloadProgressChanged += onDownloadProgressChanged;
             inception.OnDownloadCompleted += onDownloadCompleted;
 
-            Console.WriteLine("Initializing inception");
+            System.Console.WriteLine("Initializing inception");
             inception.Init();
         }
 
         private static void onDownloadProgressChanged(object sender, System.Net.DownloadProgressChangedEventArgs e)
         {
             if (e.TotalBytesToReceive <= 0)
-                Console.WriteLine(String.Format("{0} bytes downloaded", e.BytesReceived, e.ProgressPercentage));
+                System.Console.WriteLine(String.Format("{0} bytes downloaded", e.BytesReceived, e.ProgressPercentage));
             else
-                Console.WriteLine(String.Format("{0} of {1} bytes downloaded ({2}%)", e.BytesReceived, e.TotalBytesToReceive, e.ProgressPercentage));
+                System.Console.WriteLine(String.Format("{0} of {1} bytes downloaded ({2}%)", e.BytesReceived, e.TotalBytesToReceive, e.ProgressPercentage));
         }
 
         private static void onDownloadCompleted(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
@@ -47,9 +47,8 @@ namespace Inception.Console.Lite.Netstandard
             watch.Stop();
             String resStr = String.Format("Object is {0} with {1}% probability. Recognition completed in {2} milliseconds.", result[0].Label, result[0].Probability * 100, watch.ElapsedMilliseconds);
 
-
-            Console.WriteLine(resStr);
-            Console.WriteLine("Press any key to continue:");
+            System.Console.WriteLine(resStr);
+            System.Console.WriteLine("Press any key to continue:");
 
         }
     }
