94b5bbe681a6783657e0a8d3a5d338382fb5e73e
Canming Huang <supprt@emgu.com>
2019-07-10 09:08:27 -0400

    Simplify unity copy asset script on windows with wsl

--- Emgu.TF.Unity/copy_demo_assets.bat
+++ Emgu.TF.Unity/copy_demo_assets.bat
@@ -1,3 +1,1 @@
-cp ../tensorflow/tensorflow/examples/multibox_detector/data/surfers.jpg Assets/Emgu.TF/Demo/Resources
-cp ../Emgu.TF.Example/XamarinForms/XamarinForms/space_shuttle.jpg Assets/Emgu.TF/Demo/Resources
-cp unityStoreIcons/EmguTFLogo_128x128.png Assets/Emgu.TF/Demo/EmguTFLogo.png
+call wsl ./copy_demo_assets
