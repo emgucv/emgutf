eb6a806e82de7c6a18f6c767dede131318caf2f7
Canming Huang <supprt@emgu.com>
2019-09-05 10:27:34 -0400

    Display default session devices in XamarinForms demo.

--- Emgu.TF.Example/XamarinForms/XamarinForms/AboutPage.cs
+++ Emgu.TF.Example/XamarinForms/XamarinForms/AboutPage.cs
@@ -10,14 +10,50 @@ namespace Emgu.TF.XamarinForms
 {
     public class AboutPage : ContentPage
     {
+        /// <summary>
+        /// Create and run a simple graph that add two numbers and returns the default session devices used.
+        /// </summary>
+        /// <returns></returns>
+        private static Session.Device[] GetSessionDevices()
+        {
+            SessionOptions sessionOptions = null;
+            int a = 1;
+            int b = 1;
+            //Creating tensor from value a
+            Tensor tensorA = new Tensor(a);
+            //Creating tensor from value b
+            Tensor tensorB = new Tensor(b);
+            //Create a new graph
+            Graph graph = new Graph();
+            //Place holder in the graph for tensorA
+            Operation opA = graph.Placeholder(DataType.Int32, null, "valA");
+            //Place holder in the graph for tensorB
+            Operation opB = graph.Placeholder(DataType.Int32, null, "valB");
+            //Adding the two tensor
+            Operation sumOp = graph.Add(opA, opB, "sum");
+
+            //Create a new session
+            Session session = new Session(graph, sessionOptions);
+
+            //Execute the session and get the sum
+            Tensor[] results = session.Run(new Output[] { opA, opB }, new Tensor[] { tensorA, tensorB }, new Output[] { sumOp });
+
+            Session.Device[] devices = session.ListDevices(null);
+            return devices;
+        }
+
         public AboutPage()
         {
+            Session.Device[] devices = GetSessionDevices();
+            StringBuilder sb = new StringBuilder();
+            foreach (Session.Device d in devices)
+            {
+                sb.Append(String.Format("<H4 style=\"color: blue;\">{1}: {0}</H4>", d.Name, d.Type));
+            }
             String tensorflowVer = TfInvoke.Version;
             Title = "About Emgu TF";
             Content =
-            //new StackLayout
-            //{
-                //Children = {
+
                     new WebView()
                     {
                         WidthRequest =  400,
@@ -32,6 +68,7 @@ namespace Emgu.TF.XamarinForms
                     <H3> OS: {1} </H3>
                     <H3> Framework: {2} </H3>
                     <H3> Processor: {3} </H3>
+                    <H3> Default Session Devices: </H3> {4}
                     <H3> Tensorflow  <a href=https://github.com/tensorflow/tensorflow/blob/master/LICENSE > License</a> </H3>
                     <H3><a href=http://www.emgu.com/wiki/index.php/Emgu_TF >Visit our website</a> <br/><br/><H3>
                     <H3><a href=mailto:support@emgu.com>Email Support</a> <br/><br/><H3>"
@@ -41,10 +78,9 @@ namespace Emgu.TF.XamarinForms
                             tensorflowVer,
                             System.Runtime.InteropServices.RuntimeInformation.OSDescription,
                             System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription,
-                            System.Runtime.InteropServices.RuntimeInformation.ProcessArchitecture)
+                            System.Runtime.InteropServices.RuntimeInformation.ProcessArchitecture,
+                            sb.ToString())
                         }
-                    //}
-                //}
             };
         }
     }
