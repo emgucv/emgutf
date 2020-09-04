2cecf7d59a158cc3522d783f36293e870c2bf63f
Canming Huang <support@emgu.com>
2020-09-04 17:16:34 -0400

    Added script to build tf x86-64 with docker.

--- platforms/windows/docker/Docker_Build_Binary_tf.bat
+++ platforms/windows/docker/Docker_Build_Binary_tf.bat
@@ -3,5 +3,5 @@ pushd %~p0
 cd ..
 cd ..
 cd ..
-docker run -v %cd%:c:\src -w c:\src\platforms\windows\docker -m 6G emgu/vs2019_buildtools_cuda_bazel:latest Docker_Build_Helper_tf.bat %*
+docker run -v %cd%:c:\src -w c:\src\platforms\windows\docker -m 12G emgu/vs2019_buildtools_cuda_bazel:latest Docker_Build_Helper_tf.bat %*
 popd
