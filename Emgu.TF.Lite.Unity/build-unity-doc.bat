2659e7c8d808ac5d3ff3df24a53e965a6a60e9cf
Canming Huang <supprt@emgu.com>
2020-09-16 12:15:32 -0400

    Added build-unity-doc script on linux.

--- Emgu.TF.Lite.Unity/build-unity-doc.bat
+++ Emgu.TF.Lite.Unity/build-unity-doc.bat
@@ -1,10 +1,4 @@
-REM go to the folder of the current script
 pushd %~p0
-cp NamespaceDoc.cs Assets/Emgu.TF.Lite/Assets/Scripts
-wsl doxygen Emgu.TF.Lite.Doxyfile
-cd latex
-wsl make
-cd ..
-cp latex/refman.pdf ../Emgu.TF.Lite.Unity/Assets/Emgu.TF.Lite/Documentation/Emgu.TF.Lite.Documentation.pdf
-rm Assets/Emgu.TF.Lite/Assets/Scripts/NamespaceDoc.cs
+call wsl dos2unix build-unity-doc
+call wsl ./build-unity-doc
 popd
