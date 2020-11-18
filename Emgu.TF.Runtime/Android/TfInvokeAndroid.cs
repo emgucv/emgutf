5020a73509b435efb9680de6caa2aec1f8e13813
Canming Huang <supprt@emgu.com>
2020-11-18 09:29:44 -0500

    Added TfInvokeAndroid.CheckLibraryLoaded() function.

---
+++ Emgu.TF.Runtime/Android/TfInvokeAndroid.cs
@@ -0,0 +1,22 @@
+﻿//----------------------------------------------------------------------------
+//  Copyright (C) 2004-2020 by EMGU Corporation. All rights reserved.       
+//----------------------------------------------------------------------------
+
+using System;
+
+namespace Emgu.TF
+{
+   /// <summary>
+   /// TfLiteInvoke for Android
+   /// </summary>
+   public static class TfInvokeAndroid
+   {
+      /// <summary>
+      /// Return true if the class is loaded.
+      /// </summary>
+      public static bool CheckLibraryLoaded ()
+      {
+         return TfInvoke.CheckLibraryLoaded ();
+      }
+   }
+}
