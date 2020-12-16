041712eb6487ec71e6a28032649f91af3f315473
Canming Huang <support@emgu.com>
2020-12-16 16:57:08 -0500

    Update nuget packages for iOS demo

--- Emgu.TF.Example/XamarinForms/XamarinForms.Lite.iOS/Main.cs
+++ Emgu.TF.Example/XamarinForms/XamarinForms.Lite.iOS/Main.cs
@@ -7,6 +7,7 @@ namespace EmguTFLite
         // This is the main entry point of the application.
         static void Main(string[] args)
         {
+            bool loaded = Emgu.TF.Lite.TfLiteInvoke.CheckLibraryLoaded();
             // if you want to use a different Application Delegate class from "AppDelegate"
             // you can specify it here.
             UIApplication.Main(args, null, "AppDelegate");
