eb1babd666189daa6209995a807e6612533b24a1
Canming Huang <support@emgu.com>
2022-03-08 18:27:29 -0500

    Install libgdiplus for running the demos.

--- platforms/debian/bullseye/apt_install_dependency.sh
+++ platforms/debian/bullseye/apt_install_dependency.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/bin/bash -v
 
-sudo apt-get install build-essential protobuf-compiler python-future python3-future libopengl-dev
+sudo apt-get install build-essential protobuf-compiler python-future python3-future libopengl-dev libgdiplus
