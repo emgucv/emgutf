36b222fa8ebb7ef0c4e2990dd379a1d1061c23b3
Canming Huang <support@emgu.com>
2022-11-19 09:44:11 -0500

    Added documentations for AndroidFileAsset.

--- Emgu.Models/Android/AndroidFileAsset.cs
+++ Emgu.Models/Android/AndroidFileAsset.cs
@@ -16,120 +16,151 @@ using Android.Runtime;
 using Android.Views;
 
 namespace Emgu.TF.Util
-{ 
-   public abstract class AndroidFileAsset : DisposableObject
-   {
-      protected FileInfo _file;
-      protected OverwriteMethod _overwriteMethod;
+{
+    /// <summary>
+    /// Helper class for writing Android asset into a file
+    /// </summary>
+    public abstract class AndroidFileAsset : DisposableObject
+    {
+        /// <summary>
+        /// The file info
+        /// </summary>
+        protected FileInfo _file;
 
-      public enum OverwriteMethod
-      {
-         /// <summary>
-         /// Always overwrite the file
-         /// </summary>
-         AlwaysOverwrite,
-         /*
-         /// <summary>
-         /// Copy if the current file is newer than the existing one
-         /// </summary>
-         CopyIfNewer,*/
-         /// <summary>
-         /// Will never overwrite. Throw exception if the file already exist
-         /// </summary>
-         NeverOverwrite
-      }
+        /// <summary>
+        /// The file overwrite policy
+        /// </summary>
+        protected OverwritePolicy _overwritePolicy;
 
-      /// <summary>
-      /// Copy the Android assets to the app's FilesDir
-      /// </summary>
-      /// <param name="context">The android context</param>
-      /// <param name="assertName">The name of the assert</param>
-      /// <param name="dstDir">The subfolder in the app's FilesDir</param>
-      /// <param name="overwriteMethod">overwrite method</param>
-      /// <returns>The resulting FileInfo</returns>
-      public static FileInfo WritePermanentFileAsset(Context context, String assertName, String dstDir, OverwriteMethod overwriteMethod)
-      {
-         String fullPath = Path.Combine(context.FilesDir.AbsolutePath, dstDir, assertName);
 
-         //Create the directory if it is not already exist.
-         Directory.CreateDirectory(Path.GetDirectoryName(fullPath));
+        /// <summary>
+        /// The file overwrite policyt
+        /// </summary>
+        public enum OverwritePolicy
+        {
+            /// <summary>
+            /// Always overwrite the file
+            /// </summary>
+            AlwaysOverwrite,
+            /*
+            /// <summary>
+            /// Copy if the current file is newer than the existing one
+            /// </summary>
+            CopyIfNewer,*/
+            /// <summary>
+            /// Will never overwrite. Throw exception if the file already exist
+            /// </summary>
+            NeverOverwrite
+        }
 
-         using (Stream iStream = context.Assets.Open(assertName))
-            WriteStream(iStream, fullPath, overwriteMethod);
-         return new FileInfo(fullPath);
-      }
+        /// <summary>
+        /// Copy the Android assets to the app's FilesDir
+        /// </summary>
+        /// <param name="context">The android context</param>
+        /// <param name="assertName">The name of the assert</param>
+        /// <param name="dstDir">The subfolder in the app's FilesDir</param>
+        /// <param name="policy">overwrite method</param>
+        /// <returns>The resulting FileInfo</returns>
+        public static FileInfo WritePermanentFileAsset(Context context, String assertName, String dstDir, OverwritePolicy policy)
+        {
+            String fullPath = Path.Combine(context.FilesDir.AbsolutePath, dstDir, assertName);
 
-      public static void WriteStream(System.IO.Stream iStream, String fileFullPath, OverwriteMethod method)
-      {
-         if (method == OverwriteMethod.NeverOverwrite && File.Exists(fileFullPath))
-         {
-            throw new IOException(String.Format("A file with the name {0} already exist.", fileFullPath));
-         }
-         using (Stream os = File.OpenWrite(fileFullPath))
-         {
-            byte[] buffer = new byte[8 * 1024];
-            int len;
-            while ((len = iStream.Read(buffer, 0, buffer.Length)) > 0)
-               os.Write(buffer, 0, len);
-         }
-      }
+            //Create the directory if it is not already exist.
+            Directory.CreateDirectory(Path.GetDirectoryName(fullPath));
 
-      public String DirectoryName
-      {
-         get
-         {
-            return _file.DirectoryName;
-         }
-      }
+            using (Stream iStream = context.Assets.Open(assertName))
+                WriteStream(iStream, fullPath, policy);
+            return new FileInfo(fullPath);
+        }
 
-      public String FileFullPath
-      {
-         get
-         {
-            return _file.FullName;
-         }
-      }
+        /// <summary>
+        /// Write the io stream to disk
+        /// </summary>
+        /// <param name="iStream">The io stream</param>
+        /// <param name="fileFullPath">The full file path to write to</param>
+        /// <param name="policy">The overwrite policy</param>
+        /// <exception cref="IOException">Will throw exception if policy is not to overwrite and file already exist.</exception>
+        public static void WriteStream(System.IO.Stream iStream, String fileFullPath, OverwritePolicy policy)
+        {
+            if (policy == OverwritePolicy.NeverOverwrite && File.Exists(fileFullPath))
+            {
+                throw new IOException(String.Format("A file with the name {0} already exist.", fileFullPath));
+            }
+            using (Stream os = File.OpenWrite(fileFullPath))
+            {
+                byte[] buffer = new byte[8 * 1024];
+                int len;
+                while ((len = iStream.Read(buffer, 0, buffer.Length)) > 0)
+                    os.Write(buffer, 0, len);
+            }
+        }
 
-      protected override void DisposeObject()
-      {
-      }
-   }
+        /// <summary>
+        /// The directory to write the android file assets to.
+        /// </summary>
+        public String DirectoryName
+        {
+            get
+            {
+                return _file.DirectoryName;
+            }
+        }
 
-   /// <summary>
-   /// Copy the Android assets to the cache folder
-   /// </summary>
-   public class AndroidCacheFileAsset : AndroidFileAsset
-   {
-      public AndroidCacheFileAsset(Context context, String assertName, String cacheFolderPostfix)
-         : this(context, assertName, cacheFolderPostfix, OverwriteMethod.NeverOverwrite)
-      {
-      }
+        /// <summary>
+        /// The full file path of the asset 
+        /// </summary>
+        public String FileFullPath
+        {
+            get
+            {
+                return _file.FullName;
+            }
+        }
 
-      public AndroidCacheFileAsset(Context context, String assertName, String cacheFolderPostfix, OverwriteMethod method)
-      {
-         String fileName = Path.GetFileName(assertName);
-         fileName = Path.Combine(context.GetDir(cacheFolderPostfix, FileCreationMode.Private).AbsolutePath, fileName);
-         _file = new FileInfo(fileName);
-         _overwriteMethod = method;
+        /// <summary>
+        /// Release all the resources associated with this android file asset.
+        /// </summary>
+        protected override void DisposeObject()
+        {
+        }
+    }
 
-         using(System.IO.Stream iStream = context.Assets.Open(assertName))
-            WriteStream(iStream, FileFullPath, _overwriteMethod);
+    /// <summary>
+    /// Copy the Android assets to the cache folder
+    /// </summary>
+    public class AndroidCacheFileAsset : AndroidFileAsset
+    {
 
-      }
+        /// <summary>
+        /// Creat a file cache for android asset.
+        /// </summary>
+        /// <param name="context">The Android context</param>
+        /// <param name="assertName">The android asset name</param>
+        /// <param name="cacheFolderPostfix">The extra prefix to the cache folder</param>
+        /// <param name="policy">The overwrite policy</param>
+        public AndroidCacheFileAsset(Context context, String assertName, String cacheFolderPostfix = "tmp", OverwritePolicy policy = OverwritePolicy.NeverOverwrite)
+        {
+            String fileName = Path.GetFileName(assertName);
+            fileName = Path.Combine(context.GetDir(cacheFolderPostfix, FileCreationMode.Private).AbsolutePath, fileName);
+            _file = new FileInfo(fileName);
+            _overwritePolicy = policy;
 
-      public AndroidCacheFileAsset(Context context, String assertName)
-         : this(context, assertName, "tmp")
-      {
-      }
+            using (System.IO.Stream iStream = context.Assets.Open(assertName))
+                WriteStream(iStream, FileFullPath, _overwritePolicy);
 
-      protected override void DisposeObject()
-      {
-         if (_file.Exists)
-            _file.Delete();
-           
-         base.DisposeObject();
-      }
-   }
+        }
+
+        /// <summary>
+        /// Release all the resources associated with the Android File Asset.
+        /// </summary>
+        protected override void DisposeObject()
+        {
+            if (_file.Exists)
+                _file.Delete();
+
+            base.DisposeObject();
+        }
+    }
 }
 
 #endif
