601f40241cafe85b22a55b421b442daa8f6819b9
Canming Huang <support@emgu.com>
2024-07-22 08:54:52 -0400

    Use mono and nuget.exe to create nuget package on Linux

--- platforms/debian/bookworm/apt_install_dependency.sh
+++ platforms/debian/bookworm/apt_install_dependency.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -v
 
-sudo apt-get install build-essential protobuf-compiler libopengl-dev libgdiplus clang
+sudo apt-get install build-essential protobuf-compiler libopengl-dev libgdiplus clang mono-runtime libmono-system-net-http4.0-cil libmono-system-io-compression4.0-cil
