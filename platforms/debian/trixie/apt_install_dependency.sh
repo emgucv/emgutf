77225dfa8c56ec6ad6b8e9bbc1dd6742c893378a
Canming Huang <support@emgu.com>
2025-12-17 14:42:49 -0500

    Added build script for debian trixie

---
+++ platforms/debian/trixie/apt_install_dependency.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -v
+
+sudo apt-get install build-essential protobuf-compiler libopengl-dev libgdiplus clang \
+	mono-runtime mono-complete
+	
+#	libmono-system-net-http4.0-cil libmono-system-io-compression4.0-cil \
+#	libmono-system-componentmodel-composition4.0-cil \
+#	libmono-microsoft-build-utilities-v4.0-4.0-cil \
+#	libmono-system-componentmodel-dataannotations4.0-cil \
+#	libmono-windowsbase4.0-cil libmono-system-xml-linq4.0-cil \
+#	libmono-system-runtime-serialization4.0-cil
+	
